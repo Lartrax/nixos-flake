058cc9293d0b30dc231265314ec26ab2b65384b8
Lartrax <lartrax909@gmail.com>
2025-09-23 07:31:46 +0200

    move dots to flake

--- homes/cli-tools/kitty.nix
+++ homes/cli-tools/kitty.nix
@@ -2,4 +2,49 @@
 
 {
   home.packages = with pkgs; [ kitty ];
+
+  home.file.".config/kitty/theme.conf".text = ''
+    foreground              #060606
+    background              #D9D0E3
+    selection_foreground    #D9D0E3
+    selection_background    #060606
+    cursor                  #363A4C
+    cursor_text_color       #FFFFFF
+
+    active_tab_foreground     #D9D0E3
+    active_tab_background     #928AA5
+    inactive_tab_foreground   #928AA5
+    inactive_tab_background   #D9D0E3
+
+    # Black
+    color0                #000000
+    color8                #484848
+    # Red
+    color1                #CF1717
+    color9                #F07575
+    # Green
+    color2                #17CF17
+    color10               #75F075
+    # Yellow
+    color3                #CFCF17
+    color11               #F0F075
+    # Blue
+    color4                #1717CF
+    color12               #7575F0
+    # Magenta
+    color5                #CF17CF
+    color13               #F075F0
+    # Cyan
+    color6                #17CFCF
+    color14               #75F0F0
+    # White
+    color7                #EEEEEE
+    color15               #FFFFFF
+  '';
+
+  home.file.".config/kitty/kitty.conf".text = ''
+    include theme.conf
+
+    font_family "Monaspace Neon Nerd Font"
+  '';
 }
--- homes/desktop/modules/hyprlock.nix
+++ homes/desktop/modules/hyprlock.nix
@@ -2,4 +2,75 @@
 
 {
   home.packages = with pkgs; [ hyprlock ];
+
+  home.file.".config/hypr/hyprlock.conf".text = ''
+    $font = MonaspiceAr Nerd Font Propo
+
+    general {
+        hide_cursor = false
+    }
+
+    animations {
+        enabled = true
+        bezier = linear, 1, 1, 0, 0
+        animation = fadeIn, 1, 5, linear
+        animation = fadeOut, 1, 5, linear
+        animation = inputFieldDots, 1, 2, linear
+    }
+
+    background {
+        monitor =
+        path = screenshot
+        blur_passes = 3
+    }
+
+    input-field {
+        monitor =
+        size = 20%, 5%
+        outline_thickness = 3
+        inner_color = rgba(0, 0, 0, 0.0) # no fill
+
+        outer_color = rgba(D75BC1ff) rgba(EB9592ff) 45deg
+        inner_color = rgba(D9D0E3B3)
+        check_color = rgba(3A436B80) rgba(CCD5FFe6) 60deg
+        fail_color = rgba(E71B81ff) rgba(FB5552ff) 120deg
+
+        font_color = rgba(14, 14, 14, 0.2)
+        fade_on_empty = false
+        rounding = -1
+
+        font_family = $font
+        placeholder_text = Password
+        fail_text = $PAMFAIL
+
+        dots_spacing = 0.3
+        dots_color = rgba(D9D0E3)
+
+        position = 0, -20
+        halign = center
+        valign = center
+    }
+
+    label {
+        monitor =
+        text = $TIME # ref. https://wiki.hyprland.org/Hypr-Ecosystem/hyprlock/#variable-substitution
+        font_size = 90
+        font_family = $font
+
+        position = -30, 0
+        halign = right
+        valign = top
+    }
+
+    label {
+        monitor =
+        text = cmd[update:60000] date +"%A, %d %B %Y" # update every 60 seconds
+        font_size = 25
+        font_family = $font
+
+        position = -30, -150
+        halign = right
+        valign = top
+    }
+  '';
 }
