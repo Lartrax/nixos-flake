04ed6cf3fbe67e1810970903469eb1088f22d1d0
Lartrax <lartrax909@gmail.com>
2025-09-23 21:57:19 +0200

    use latest kernel

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -3,6 +3,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [ "quiet" ];
 
   networking.networkmanager.enable = true;
