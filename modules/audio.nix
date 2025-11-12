7d981781fe47a08c1f0b2e8bae0844c07d6e1d3e
Lartrax <lartax909@gmail.com>
2025-11-12 17:58:51 +0100

    disable pulseaudio and lower audio latency

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,6 +1,7 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -8,5 +9,14 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+
+    extraConfig.pipewire."48-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 32;
+        "default.clock.min-quantum" = 16;
+        "default.clock.max-quantum" = 64;
+      };
+    };
   };
 }
