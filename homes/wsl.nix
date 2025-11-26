319bba31effa0a173df354ca0cffe376039aa026
Lartrax <lartax909@gmail.com>
2025-11-26 14:49:45 +0100

    add go and cloud-sql-proxy

--- homes/wsl.nix
+++ homes/wsl.nix
@@ -6,7 +6,9 @@
     ./cli-tools/git.nix
     ./cli-tools/lazygit.nix
     ./cli-tools/google-cloud-sdk.nix
+    ./cli-tools/google-cloud-sql-proxy.nix
     ./cli-tools/terraform.nix
+    ./cli-tools/go.nix
   ];
 
   home.stateVersion = "25.05";
