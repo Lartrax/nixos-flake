f39e0d7483ab446b7c992b8c89cfa6bf211b338c
Lartrax <lartax909@gmail.com>
2025-12-03 11:25:08 +0100

    add new packages to wsl

--- homes/wsl.nix
+++ homes/wsl.nix
@@ -9,6 +9,10 @@
     ./cli-tools/google-cloud-sql-proxy.nix
     ./cli-tools/terraform.nix
     ./cli-tools/go.nix
+    ./cli-tools/bitwarden-cli.nix
+    ./cli-tools/gopass.nix
+    ./cli-tools/sql-migrate.nix
+    ./cli-tools/gnumake.nix
   ];
 
   home.stateVersion = "25.05";
