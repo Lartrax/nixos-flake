9a628ca8283d3b4a0069bb070c0254b3457f78a6
Lartrax <lartax909@gmail.com>
2026-02-28 13:35:22 +0100

    revert to open nvidia drivers with cachyos patches applied

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -27,11 +27,24 @@
     # Turns off gpu when not in use, experimental
     powerManagement.finegrained = false;
     # Use the Nvidia open source kernel module
-    open = false;
+    open = true;
     # Enable the Nvidia settings menu
     nvidiaSettings = true;
     # Select driver version
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = let
+      base = config.boot.kernelPackages.nvidiaPackages.latest;
+      cachyos-nvidia-patch = pkgs.fetchpatch {
+        url =
+          "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
+        sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+      };
+
+      driverAttr = if config.hardware.nvidia.open then "open" else "bin";
+    in base // {
+      ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
+        patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
+      });
+    };
   };
 
   environment.sessionVariables = {
